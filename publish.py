220ca1c2d5b32be3f1f8a358541cc62c1f0a98f5
Garrett Smith <garrett@placeon.earth>
2024-01-12 07:29:40 -0600

    Implement publish

--- publish.py
+++ publish.py
@@ -9,9 +9,17 @@ log = logging.getLogger()
 
 DATA_JSON = "data.json"
 
+remote = "gage"
+bucket = "gage-boards"
+board_key = "open-llm"
+test_credentials = False
+
+target_obj = f"{remote}:{bucket}/{board_key}.json"
+
 
 def main():
     _check_rclone()
+    _check_credentials()
     board = _generate_board()
     _save_board(board)
     _publish_board()
@@ -34,6 +42,27 @@ def _check_rclone():
         log.info("Found %s", out.split("\n")[0])
 
 
+def _check_credentials():
+    log.info("Checking remote credentials")
+    try:
+        subprocess.run(
+            f"rclone touch {target_obj}",
+            shell=True,
+            text=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        raise SystemExit(
+            "There's a problem with the remote configuration. Confirm that "
+            "your environment is setup with the required credentials and that "
+            f"you have permission to write to {target_obj}"
+        )
+    else:
+        if test_credentials:
+            log.info("Credentials are OK")
+            raise SystemExit(0)
+
+
 def _generate_board():
     log.info("Generating board using board.json")
     return subprocess.check_output(
@@ -50,7 +79,20 @@ def _save_board(board):
 
 
 def _publish_board():
-    pass
+    log.info("Publishing board to %s", target_obj)
+    try:
+        subprocess.run(
+            f"rclone copyto {DATA_JSON} {target_obj}",
+            shell=True,
+            text=True,
+            check=True,
+        )
+    except subprocess.CalledProcessError as e:
+        log.error(
+            "There was an error publishing the board. Refer to the "
+            "output above for details."
+        )
+        raise SystemExit(e.returncode)
 
 
 if __name__ == "__main__":
